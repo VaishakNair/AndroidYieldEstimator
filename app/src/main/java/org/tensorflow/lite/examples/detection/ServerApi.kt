1c5fb3b9554490f4155ef9af27644c1800f848e5
VaishakNair <vaishak.k.nair@gmail.com>
2022-05-04 12:34:43 +0530

    Increase read/ write timeouts of Http client.

--- app/src/main/java/org/tensorflow/lite/examples/detection/ServerApi.kt
+++ app/src/main/java/org/tensorflow/lite/examples/detection/ServerApi.kt
@@ -2,11 +2,12 @@ package org.tensorflow.lite.examples.detection
 
 
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
+import java.util.concurrent.TimeUnit
 
 
 data class ServerResponse(val message: String)
@@ -29,7 +30,15 @@ interface ServerApi {
             "http://192.168.18.6:3000/" // TODO Change according to server location
 
         fun create(): ServerApi {
+
+            val okHttpClient = OkHttpClient().newBuilder()
+                .connectTimeout(40, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.MINUTES)
+                .writeTimeout(60, TimeUnit.MINUTES)
+                .build()
+
             val retrofit = Retrofit.Builder()
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
                 .build()
